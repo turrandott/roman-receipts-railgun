1d07374f9b5ba259d246dca36a35c89e77ff898f
AriaWorkshop <ariaworkshopnft@gmail.com>
2023-10-07 12:10:38 +0200

    profile

--- packages/nextjs/pages/profile.tsx
+++ packages/nextjs/pages/profile.tsx
@@ -2,18 +2,19 @@ import React, { useEffect, useState } from 'react';
 import { RequestNetwork, Types } from "@requestnetwork/request-client.js";
 
 import { formatUnits } from "viem";
+import { useAccount } from 'wagmi';
 
 // EDIT THIS TO SELECT THE USER'S ADDRESS
-const userAddress = "0x519145B771a6e450461af89980e5C17Ff6Fd8A92";
+
 
 
 const Profile = () => {
   const [activeButton, setActiveButton] = useState('');
 
-
+const {address} = useAccount();
   //get all history
 
-  const [requests, setRequests] =
+  const [totalRequestHistory, SetTotalRequestHistory] =
   useState<(Types.IRequestDataWithEvents | undefined)[]>();
 useEffect(() => {
   const requestClient = new RequestNetwork({
@@ -24,19 +25,37 @@ useEffect(() => {
   requestClient
     .fromIdentity({
       type: Types.Identity.TYPE.ETHEREUM_ADDRESS,
-      value: userAddress,
+      value: address as string,
     })
     .then((requests) => {
-      setRequests(requests.map((request) => request.getData()));
+        SetTotalRequestHistory(requests.map((request) => request.getData()));
     });
-}, []);
+}, [address]);
 
-
+const calculateStatus = (
+    state: string,
+    expectedAmount: bigint,
+    balance: bigint
+  ) => {
+    if (balance >= expectedAmount) {
+      return "Paid";
+    }
+    if (state === Types.RequestLogic.STATE.ACCEPTED) {
+      return "Accepted";
+    } else if (state === Types.RequestLogic.STATE.CANCELED) {
+      return "Canceled";
+    } else if (state === Types.RequestLogic.STATE.CREATED) {
+      return "Created";
+    } else if (state === Types.RequestLogic.STATE.PENDING) {
+      return "Pending";
+    }
+  };
   //set totalHistory to results
   //filter results based on request.balance.balance 
   //if request.balance.balance > 0 => !pendingInvoice
 
   return (
+    <div>
     <div className="p-4">
       <button
         className={`mr-4 py-2 px-4 border rounded ${activeButton === 'pendingInvoices' ? 'bg-primary text-white' : 'bg-gray-200 text-black'}`}
@@ -51,6 +70,69 @@ useEffect(() => {
       >
         Transaction History
       </button>
+    </div>
+    {activeButton === "transactionHistory" &&
+      <div className="App p-8 bg-gray-100 min-h-screen">
+      
+      <div className="bg-white p-4 rounded-lg shadow-md">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th>Timestamp</th>
+              <th>Request ID</th>
+              <th>Payer</th>
+              <th>Currency</th>
+              <th>Expected Amount</th>
+              <th>Reason</th>
+              <th>Due Date</th>
+              <th>Status</th>
+              <th>Balance</th>
+            </tr>
+          </thead>
+          <tbody>
+          {totalRequestHistory
+  ?.filter((request : any)  => {
+    return calculateStatus(
+      request.state,
+      BigInt(request.expectedAmount),
+      BigInt(request.balance?.balance || 0)
+    ) === "Paid";
+  })
+  .map((request: any) => (
+    <tr key={request.timestamp}>
+      <td>{request.timestamp}</td>
+
+      <td >
+        {request.requestId.slice(0, 4)}...
+        {request.requestId.slice(62, 66)}
+      </td>
+      <td>
+        {request.payer?.value.slice(0, 5)}...
+        {request.payer?.value.slice(39, 42)}
+      </td>
+      <td>{request.currency}</td>
+      <td>{formatUnits(BigInt(request.expectedAmount), 18)}</td>
+      <td>{request.contentData.reason}</td>
+      <td>{request.contentData.dueDate}</td>
+      <td>
+        {calculateStatus(
+          request.state,
+          BigInt(request.expectedAmount),
+          BigInt(request.balance?.balance || 0)
+        )}
+      </td>
+      <td>{formatUnits(BigInt(request.balance?.balance || 0), 18)}</td>
+    </tr>
+  ))}
+          </tbody>
+        </table>
+      </div>
+  
+      <h1 className="text-2xl font-bold mt-8 mb-4">Raw Requests</h1>
+     
+    </div>
+    }
+  
     </div>
   );
 }
