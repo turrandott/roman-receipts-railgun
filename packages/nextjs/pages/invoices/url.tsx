d11dfedd71821d93e981530bd3d3ab8308126512
Tatiana <tarzhakova@gmail.com>
2023-10-08 04:49:08 +0200

    url bug fixing

--- packages/nextjs/pages/invoices/url.tsx
+++ packages/nextjs/pages/invoices/url.tsx
@@ -8,15 +8,27 @@ import QRCode from "react-qr-code";
 export default function Url() {
   const { asPath } = useRouter();
 
-  const fullUrl = `${window.location.protocol}//${window.location.host}${asPath}`;
+  const getFullUrl = () => {
+    // if (typeof window !== 'undefined') {
+    //   return window ? `${window.location.protocol}//${window.location.host}${asPath}` : "";
+    // }
+    // return null;
+    return `http://localhost:3000/${asPath}`;
+  };
 
-  return (
-    <div className="flex flex-col justify-center items-center">
-      <span className="mb-2 break-all text-sm text-gray-400" onClick={e => copyToClipboard(fullUrl)}>
-        {fullUrl}
-      </span>
-      {/* <span><CopyIcon className="h-4" /></span> */}
-      <QRCode value={fullUrl} size={150} className="mt-8" />
-    </div>
-  );
+  const fullUrl = getFullUrl();
+
+  if (fullUrl) {
+    return (
+      <div className="flex flex-col justify-center items-center">
+        <span className="mb-2 break-all text-sm text-gray-400" onClick={e => copyToClipboard(fullUrl)}>
+          {fullUrl}
+        </span>
+        {/* <span><CopyIcon className="h-4" /></span> */}
+        <QRCode value={fullUrl} size={150} className="mt-8" />
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
